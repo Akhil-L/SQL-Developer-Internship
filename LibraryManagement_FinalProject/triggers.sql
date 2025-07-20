6588db976a76d3c487875a4a5f81f4cd14d7bbd7
Akhil-L <alakshup@gitam.in>
2025-07-20 12:55:26 +0530

    Add files via upload

---
+++ LibraryManagement_FinalProject/triggers.sql
@@ -0,0 +1,15 @@
+-- triggers.sql: Trigger for Due-Date Notification
+
+DELIMITER //
+
+CREATE TRIGGER NotifyDueDate
+AFTER INSERT ON Loans
+FOR EACH ROW
+BEGIN
+  DECLARE dueDate DATE;
+  SET dueDate = DATE_ADD(NEW.LoanDate, INTERVAL 14 DAY);
+  SELECT CONCAT('🔔 Book ID ', NEW.BookID, ' is due on ', dueDate) AS DueNotification;
+END;
+//
+
+DELIMITER ;
